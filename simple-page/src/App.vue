a2c35574cbbf24ecbca4159b2ab230b3f7d0c6cc
Fernando Washington <ironitabarbosa@gmail.com>
2025-04-11 22:23:27 -0400

    feat: implement hero component to App

--- simple-page/src/App.vue
+++ simple-page/src/App.vue
@@ -2,15 +2,20 @@
     <header>
         <AppHeader />
     </header>
+    <main>
+        <AppHero />
+    </main>
 </template>
 
 <script>
     import AppHeader from "./components/AppHeader.vue";
+    import AppHero from "./components/AppHero.vue";
 
     export default {
     name: "App",
     components: {
     AppHeader,
+    AppHero,
     },
     };
 </script>

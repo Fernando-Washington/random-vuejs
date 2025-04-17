4f4b8c4a15a70e51073dca0aafc4b40fa7fb8296
Fernando Washington <ironitabarbosa@gmail.com>
2025-04-16 22:16:48 -0400

    feat: implement AppInfo to App

--- simple-page/src/App.vue
+++ simple-page/src/App.vue
@@ -5,17 +5,20 @@
     <main>
         <AppHero />
     </main>
+        <AppInfo />
 </template>
 
 <script>
     import AppHeader from "./components/AppHeader.vue";
     import AppHero from "./components/AppHero.vue";
+    import AppInfo from "./components/AppInfo.vue";
 
     export default {
     name: "App",
     components: {
     AppHeader,
     AppHero,
+    AppInfo,
     },
-    };
+};
 </script>

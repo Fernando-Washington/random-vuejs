834f73f9c63d690912a6f82d599b07dbe1e4fa99
Fernando Washington <ironitabarbosa@gmail.com>
2025-04-18 20:31:26 -0400

    chore(AboutHydra): format code

--- simple-page/src/components/AboutHydra.vue
+++ simple-page/src/components/AboutHydra.vue
@@ -1,33 +1,34 @@
 <template>
-<div class="about-container">
-        <div class="image-container">
-        <img :src="virtualRealityMan" alt="" />
-        </div>
-        <div class="text-container">
-        <div class="title-container">
-            <h2>
-            <span class="bold-text"> About </span>
-            </h2>
-            <h2>
-            <span class="thin-text"> Hydra vr </span>
-            </h2>
-        </div>
-        <div class="text-box">
-            <p>
-            Eget mi proin sed libero enim sed faucibus turpis. Nisl rhoncus mattis
-            rhoncus urna neque viverra justo. Vivamus at augue eget arcu dictum.
-            Ultrices lastre dictum fusce ut placerat orci. Aenean et tortor at risus
-            viverra adipiscing at in. Mattis aliquam faucibus purus in massa. Est
-            placerat in egestas erat imperdiet sed. Consequat semper viverra nam
-            libero justo laoreet sit amet. Aliquam etiam erat velit scelerisque in
-            dictum non consectetur a. Laoreet sit amet cursus sit amet. Vel eros
-            donec ac odio tempor orci dapibus. Sem nulla pha retra diam sit amet
-            nisl suscipit adipiscing bibendum. Leo a diam sollicitudi n tempor.
-            </p>
-        </div>
-        <a class="btn" href="#">Let's get in touch</a>
-        </div>
+  <div class="about-container">
+    <div class="image-container">
+      <img :src="virtualRealityMan" alt="" />
     </div>
+    <div class="text-container">
+      <div class="title-container">
+        <h2>
+          <span class="bold-text"> About </span>
+        </h2>
+        <h2>
+          <span class="thin-text"> Hydra vr </span>
+        </h2>
+      </div>
+      <div class="text-box">
+        <p>
+          Eget mi proin sed libero enim sed faucibus turpis. Nisl rhoncus mattis
+          rhoncus urna neque viverra justo. Vivamus at augue eget arcu dictum.
+          Ultrices lastre dictum fusce ut placerat orci. Aenean et tortor at
+          risus viverra adipiscing at in. Mattis aliquam faucibus purus in
+          massa. Est placerat in egestas erat imperdiet sed. Consequat semper
+          viverra nam libero justo laoreet sit amet. Aliquam etiam erat velit
+          scelerisque in dictum non consectetur a. Laoreet sit amet cursus sit
+          amet. Vel eros donec ac odio tempor orci dapibus. Sem nulla pha retra
+          diam sit amet nisl suscipit adipiscing bibendum. Leo a diam
+          sollicitudi n tempor.
+        </p>
+      </div>
+      <a class="btn" href="#">Let's get in touch</a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -44,15 +45,13 @@ export default {
 </script>
 
 <style>
-
 .about-container {
-    display: flex;
+  display: flex;
 }
 
 .bold-text {
-    position: relative;
+  position: relative;
 }
-
 
 .text-container > .btn {
   display: inline-block;
